29cf4ba3f0560c14efcd5c005495036a492f7fa1
Luciano Santos <luciano_z7@hotmail.com>
2023-03-14 23:48:29 -0300

    Pacotes

---
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever registra uma msg
+func Escrever() {
+	fmt.Println("Escrevendo dp pacote auxiliar")
+	escrever2()
+}
---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever registra uma msg
+func escrever2() {
+	fmt.Println("Escrevendo dp pacote auxiliar 2")
+}
